33a6bd89065a3abb66c2e0dd5e3f34220a4c3347
Millan <real.millan.wang@gmail.com>
2022-01-24 21:50:22 -0500

    Initializing repository

---
+++ 3303Elevator/src/FirstClassToSetupRepo.java
@@ -0,0 +1,14 @@
+/**
+ * Big javadoc documentations
+ * @author Millan
+ *
+ */
+public class FirstClassToSetupRepo {
+	/**
+	 * Temporary base class to initialize the repository
+	 * @param args Command line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("I love elevators");
+	}
+}
