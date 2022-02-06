11ca2b5cbe25ed36340db8b3f74cfce7b5520525
Abdelrahim-K <abdelrahim1702@gmail.com>
2022-02-05 20:22:38 -0500

    Added testing for Time Management System and adjusted returns to milliseconds instead of seconds

--- 3303Elevator/src/app/Scheduler/TimeManagementSystem.java
+++ 3303Elevator/src/app/Scheduler/TimeManagementSystem.java
@@ -4,32 +4,53 @@
 package app.Scheduler;
 
 /**
- * @author Abdelrahim Karaja
+ * SYSC 3303, Final Project Iteration 1&0
+ * TimeManagementSystem.java
+ * 
  * Time manager for elevator movement between floors and time spent loading/unloading passengers
+ * 
+ * @author Abdelrahim Karaja
+ * 
  */
 import java.util.Random;
 import java.util.ArrayList;
 public class TimeManagementSystem {
 	public float timeMultiplier; //Used to scale time for testing purposes
-	
+		
+	/**
+	* Constructor for TimeManagementSystem Class
+	* 
+	* @param multiplier time multiplier for testing purposes
+	*/
 	public TimeManagementSystem(float multiplier){
-		this.timeMultiplier = multiplier;
+		this.timeMultiplier = Math.abs(multiplier);
 	}
 	
+	/**
+	 * Elevator loading time generator
+	 * Generates a random float value in milliseconds for time while elevator door is opened.
+	 * 
+	 * @return single float time value
+	 */
 	public float getElevatorLoadingTime() {
-		//Generates a random value in seconds for time while elevator door is opened.
 		Random r = new Random();
-		return timeMultiplier * (7.92f + r.nextFloat() * 3.22f);
+		return timeMultiplier * ((7.92f + r.nextFloat() * 3.22f) * 1000);
 	}
 	
+	/**
+	 * Function to generate random array of times within predetermined range, one for each level of movement.
+	 * @param currentFloor current user floor
+	 * @param destinationFloor final destination floor for user
+	 * @return ArrayList of float time values
+	 */
 	public ArrayList<Float> getElevatorTransitTime(int currentFloor, int destinationFloor) {
-		//Generates array of times for travel between floors
+		//Create array of times for travel between floors
 		Random r = new Random();
 		ArrayList<Float> times = new ArrayList<>();
 		
 		//Generate array of times from floor to floor
 		for(int i = 0; i < destinationFloor - currentFloor; i++) {
-			times.add(timeMultiplier * (2.77f + r.nextFloat() * 1.07f));
+			times.add(timeMultiplier * ((2.77f + r.nextFloat() * 1.07f) * 1000)); //Add each time
 		}
 		return times;
 	}
