5245b36a20c34356fc9e6668bc5f629c062c7047
Millan <real.millan.wang@gmail.com>
2022-03-12 23:30:39 -0500

    Scheduler test update. Defeated

--- 3303Elevator/src/tests/SchedulerTests/SchedulerTests.java
+++ 3303Elevator/src/tests/SchedulerTests/SchedulerTests.java
@@ -1,8 +1,8 @@
 package tests.SchedulerTests;
 
-
-
-
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -10,6 +10,9 @@ import org.junit.Test;
 
 import app.Logger;
 import app.Config.Config;
+import app.ElevatorSubsystem.Direction.Direction;
+import app.ElevatorSubsystem.Elevator.ElevatorInfo;
+import app.ElevatorSubsystem.StateMachine.ElevatorStateMachine;
 import app.FloorSubsystem.FloorSubsystem;
 import app.FloorSubsystem.ScheduledElevatorRequest;
 import app.Scheduler.Scheduler;
@@ -38,8 +41,29 @@ public class SchedulerTests {
 	@Test
 	public void testFloorSystemScheduleRequestCanGetPickedUp_AShape_SplitRequests() {
 		//Schedule trip from 1 to 3 and 
-//		scheduler.floorSystemScheduleRequest(new ScheduledElevatorRequest(null,1,false,3));
-//		Assert.assertFalse(scheduler.getNextFloorsToVisit(1, true).isEmpty());
+		ArrayList<ScheduledElevatorRequest> requests = new ArrayList<ScheduledElevatorRequest>();
+		requests.add(new ScheduledElevatorRequest(null,1,true,3));
+		requests.add(new ScheduledElevatorRequest(null,2,true,5));
+		scheduler.floorSystemScheduleRequest(requests);
+		
+		int elevatorID=1;
+		int floor=1;
+		
+		//TESTING MULTITHREADINGLY IS OUTSIDE OF THE CURRENT TIME BUDGET :(
+		/*
+		 * Because of wait loops, testing calling those methods on scheduler would require multiple threads
+		 * Gotta figure out a way to do multithreaded JUnit testing
+		 * */
+		
+		LinkedList<ElevatorInfo> elevatorSubsystemComms = new LinkedList<ElevatorInfo>();
+		elevatorSubsystemComms.add(new ElevatorInfo(elevatorID, floor, ElevatorStateMachine.Idle, Direction.UP));
+		this.scheduler.setAllElevatorInfo(elevatorSubsystemComms);
+		
+		
+		//Has a next floor to visit
+		Assert.assertFalse(scheduler.getNextFloorsToVisit().get(elevatorID)!=-1);
+		
+		
 //		Assert.assertFalse(scheduler.getNextFloorsToVisit(2, true).isEmpty());
 //		//Once we reach the top, no more next floors to visit
 //		Assert.assertTrue(scheduler.getNextFloorsToVisit(3, true).isEmpty());
@@ -51,79 +75,4 @@ public class SchedulerTests {
 //		//Once we reach the bottom, no more next floors to visit
 //		Assert.assertTrue(scheduler.getNextFloorsToVisit(1, true).isEmpty());
 	}
-	
-	/**
-	 * Testing the scheduler returning a set of destinations that need to be reached given Input 
-	 * object parameters
-	 * 
-	 * Path is an A shape with commands all at beginning
-	 */
-	@Test
-	public void testFloorSystemScheduleRequestCanGetPickedUp_AShape_UpFrontRequests() {
-		//Schedule trip from 1 to 3 and 
-//		scheduler.floorSystemScheduleRequest(new ScheduledElevatorRequest(null,1,false,3));
-//		scheduler.floorSystemScheduleRequest(new ScheduledElevatorRequest(null,3,true,1));
-//		scheduler.floorSystemScheduleRequest(new ScheduledElevatorRequest(null,2,true,1));
-//		
-//		Assert.assertFalse(scheduler.getNextFloorsToVisit(1, true).isEmpty());
-//		Assert.assertFalse(scheduler.getNextFloorsToVisit(2, true).isEmpty());
-//		
-//		//Once we reach the top,  next floors to visit will now be downwards
-//		Assert.assertFalse(scheduler.getNextFloorsToVisit(3, true).isEmpty());
-//		
-//		Assert.assertFalse(scheduler.getNextFloorsToVisit(3, false).isEmpty());
-//		Assert.assertFalse(scheduler.getNextFloorsToVisit(2, false).isEmpty());
-//		//Once we reach the bottom, no more next floors to visit
-//		Assert.assertTrue(scheduler.getNextFloorsToVisit(1, true).isEmpty());
-	}
-	
-	/**
-	 * Test scheduler returning a set of destinations to be reached 
-	 * given source and destination parameters
-	 * 
-	 * Path is an A shape with commands coming incrementally
-	 */
-	@Test
-	public void testAddRequestCanGetPickedUp_AShape_SplitRequests() {
-		//Schedule trip from 1 to 2
-		scheduler.addElevatorRequest(1,2);
-//		Assert.assertFalse(scheduler.getNextFloorsToVisit(1, true).isEmpty());
-//		//Once we reach the top, no more next floors to visit
-//		Assert.assertTrue(scheduler.getNextFloorsToVisit(2, true).isEmpty());
-//		
-//		//Schedule other trips 3 to 1 and 2 to 1
-//		scheduler.addElevatorRequest(3,1);
-//		scheduler.addElevatorRequest(2,1);
-//		Assert.assertFalse(scheduler.getNextFloorsToVisit(3, false).isEmpty());
-//		Assert.assertFalse(scheduler.getNextFloorsToVisit(2, false).isEmpty());
-//		//Once we reach the bottom, no more next floors to visit
-//		Assert.assertTrue(scheduler.getNextFloorsToVisit(1, true).isEmpty());
-	}
-	
-	
-	/**
-	 * Testing the scheduler returning a set of destinations that need to be reached
-	 *  given start and destination integer parameters
-	 * 
-	 * Path is an A shape with commands all at beginning
-	 */
-	@Test
-	public void testAddRequestCanGetPickedUp_AShape_UpFrontRequests() {
-		//Schedule trip from 1 to 3 and 
-		scheduler.addElevatorRequest(3,1);
-		scheduler.addElevatorRequest(2,1);
-		scheduler.addElevatorRequest(1,2);
-//		
-//		Assert.assertFalse(scheduler.getNextFloorsToVisit(1, true).isEmpty());
-//		Assert.assertFalse(scheduler.getNextFloorsToVisit(2, true).isEmpty());
-//		
-//		//Once we reach the top,  next floors to visit will now be downwards
-//		Assert.assertFalse(scheduler.getNextFloorsToVisit(3, true).isEmpty());
-//		
-//		Assert.assertFalse(scheduler.getNextFloorsToVisit(3, false).isEmpty());
-//		Assert.assertFalse(scheduler.getNextFloorsToVisit(2, false).isEmpty());
-//		//Once we reach the bottom, no more next floors to visit
-//		Assert.assertTrue(scheduler.getNextFloorsToVisit(1, true).isEmpty());
-	}
-	
 }
