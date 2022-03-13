b9bd73cfc73444f0050a0fde49fb6b09440ffaf5
Abdelrahim-K <abdelrahim1702@gmail.com>
2022-03-12 20:44:54 -0500

    Added a server logger and altered logger

--- 3303Elevator/src/app/Logger.java
+++ 3303Elevator/src/app/Logger.java
@@ -8,19 +8,31 @@
 package app;
 
 import java.time.LocalTime;
+import java.util.LinkedList;
 
 import app.Config.Config;
 import app.ElevatorSubsystem.Direction.Direction;
+import app.ElevatorSubsystem.Elevator.ElevatorInfo;
 import app.FloorSubsystem.ScheduledElevatorRequest;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import app.UDP.PacketReceiver;
+import app.UDP.Util;
 
-public class Logger {
+public class Logger extends Util{
 
 	private LocalTime dateTime; 
 	private boolean printElevatorEvents;
 	private boolean printSchedulerEvents; 
 	private boolean printFloorEvents; 
 	private boolean printTimeManagementSystemEvent; 
-	
+	protected Config config;
+	private InetSocketAddress sLoggerAddr;
 	/**
 	 * Constructor selects the events types to be printed through the boolean parameters  
 	 * 
@@ -34,7 +46,13 @@ public class Logger {
 		this.printElevatorEvents = c.getBoolean("elevator.log");
 		this.printSchedulerEvents = c.getBoolean("scheduler.log"); 
 		this.printFloorEvents = c.getBoolean("floor.log"); 
-		this.printTimeManagementSystemEvent = c.getBoolean("time.log"); 
+		this.printTimeManagementSystemEvent = c.getBoolean("time.log");
+		this.config = c;
+		try {
+			this.sLoggerAddr = new InetSocketAddress(config.getString("logger.address"), config.getInt("logger.port"));
+		}catch(Exception e) {
+			System.exit(1);
+		}
 	}
 	
 	/**
@@ -45,7 +63,12 @@ public class Logger {
 	 */
 	public void logElevatorEvents(String message) {
 		if(printElevatorEvents == true) {
-			System.out.println("[" + dateTime.now() + "]" + message);
+			String m = "[" + dateTime.now() + "]" + message;
+			System.out.println(m);
+			if(sLoggerAddr.getAddress().toString() != "localHost") { //if live, send to server logger
+				DatagramPacket packet = buildLoggerPacket(m);
+				Util.sendRequest_ReturnReply(packet);
+			}
 		}
 	}
 	
@@ -57,7 +80,12 @@ public class Logger {
 	 */
 	public void logFloorEvent(ScheduledElevatorRequest request) {
 		if(printFloorEvents == true) {
-			System.out.println(dateTime.now()+ " floor number " +  request.getStartFloor() + " logged a floor request at time " +request.getTime() + "  to floor " +request.getDestinationFloor());
+			String m = (dateTime.now()+ " floor number " +  request.getStartFloor() + " logged a floor request at time " +request.getTime() + "  to floor " +request.getDestinationFloor());
+			System.out.println(m);
+			if(sLoggerAddr.getAddress().toString() != "localHost") { //if live, send to server logger
+				DatagramPacket packet = buildLoggerPacket(m);
+				Util.sendRequest_ReturnReply(packet);
+			}
 		}
 		
 	}
@@ -69,7 +97,12 @@ public class Logger {
 	 */
 	public void logSchedulerEvent(String schedulerLogString) {
 		if(printSchedulerEvents == true) {
-			System.out.println(dateTime.now()+ " " + schedulerLogString);
+			String m = (dateTime.now()+ " " + schedulerLogString);
+			System.out.println(m);
+			if(sLoggerAddr.getAddress().toString() != "localHost") { //if live, send to server logger
+				DatagramPacket packet = buildLoggerPacket(m);
+				Util.sendRequest_ReturnReply(packet);
+			}
 		}
 	}
 	
@@ -80,8 +113,30 @@ public class Logger {
 	 */
 	public void logTimeManagementSystemEvent(String timeManagementLogString) {
 		if(printTimeManagementSystemEvent == true) {
-			System.out.println(dateTime.now() + " " + timeManagementLogString);
+			String m = (dateTime.now() + " " + timeManagementLogString);
+			System.out.println(m);
+			if(sLoggerAddr.getAddress().toString() != "localHost") { //if live, send to server logger
+				DatagramPacket packet = buildLoggerPacket(m);
+				Util.sendRequest_ReturnReply(packet);
+			}
 		}
 	}
 	
+	private DatagramPacket buildLoggerPacket(String m){
+		byte[] data = {};
+		
+		try {
+			data = Util.serialize(m);
+		}catch(IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		return new DatagramPacket(
+				data,
+				data.length,
+				sLoggerAddr.getAddress(),
+				sLoggerAddr.getPort()
+		);
+	}
 }
