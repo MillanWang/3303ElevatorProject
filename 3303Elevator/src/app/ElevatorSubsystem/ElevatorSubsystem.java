dad2cdc1ae2dc28bacf0fcccfec32badef72af59
benk <benkittilsen@gmail.com>
2022-03-26 19:49:52 -0400

    disable the logging spam

--- 3303Elevator/src/app/ElevatorSubsystem/ElevatorSubsystem.java
+++ 3303Elevator/src/app/ElevatorSubsystem/ElevatorSubsystem.java
@@ -54,7 +54,6 @@ public class ElevatorSubsystem implements Runnable{
 	}
 
 	private DatagramPacket buildSchedulerPacket(){
-		this.log("get elevators status");
 		LinkedList<ElevatorInfo> list = this.statusBuf.getAllStatus();
 
 		byte[] data = {};
@@ -96,13 +95,16 @@ public class ElevatorSubsystem implements Runnable{
 	@SuppressWarnings("unchecked")
 	public void run(){
 		this.createElevators();
+		boolean disableLog = false;
 		while(true) {
-			this.log("building packet");
+			if(!disableLog) this.log("building packet");
 			DatagramPacket sendPacket = this.buildSchedulerPacket();
-			this.log("sending status to scheduler");
+			if(!disableLog) this.log("sending status to scheduler");
 			DatagramPacket recievedPacket = Util.sendRequest_ReturnReply(sendPacket);
-			this.log("receieved packet from scheduler");
+			if(!disableLog) this.log("receieved packet from scheduler");
 
+			disableLog = false;
+			
 			HashMap<Integer, Integer> info = null;
 
 			try {
@@ -115,27 +117,33 @@ public class ElevatorSubsystem implements Runnable{
 			}
 
 			if(info != null) {
-				int count = 0;
+				
+				int dec = 0;
 				for(int i = 0; i < this.numElevators; i++) {
 					if(!info.containsKey(i+1)) {
 						info.put(i+1, -1);
 					}else if(info.get(i+1) == -3){
+						dec++;
+					}
+				}
+				
+				int count = 0;
+				for(int i = 0; i < this.numElevators; i++) {
+					if(info.get(i+1) == -1) {
 						count++;
 					}
 				}
-
-				for(int i = 0; i < this.numElevators; i++) {
-					System.out.println((i+1) + " " + info.get(i+1));
+				
+				if(count == this.numElevators) {
+					disableLog = true;
 				}
 				
-				this.numElevators -= count;
+				this.numElevators -= dec;
 				this.nextFloorBuf.addReq(info);
 				
 				if(this.numElevators == 0) {
 					return;
 				}
-			}else {
-				System.out.println("Elevator info is null");
 			}
 		}
 	}
