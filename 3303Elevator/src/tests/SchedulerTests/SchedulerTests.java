7c7f2e8f0595ed90904b99486aea879a5b50dffb
Millan <real.millan.wang@gmail.com>
2022-04-11 21:59:39 -0400

    Updated scheduler tests

--- 3303Elevator/src/tests/SchedulerTests/SchedulerTests.java
+++ 3303Elevator/src/tests/SchedulerTests/SchedulerTests.java
@@ -1,8 +1,8 @@
 package tests.SchedulerTests;
 
-import java.util.ArrayList;
+import java.net.DatagramPacket;
+import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -10,16 +10,15 @@ import org.junit.Test;
 
 import app.Logger;
 import app.Config.Config;
-import app.ElevatorSubsystem.Direction.Direction;
 import app.ElevatorSubsystem.Elevator.ElevatorInfo;
-import app.ElevatorSubsystem.StateMachine.ElevatorStateMachine;
-import app.FloorSubsystem.FloorSubsystem;
 import app.FloorSubsystem.ScheduledElevatorRequest;
 import app.Scheduler.Scheduler;
+import app.UDP.PacketReceiver;
 
 public class SchedulerTests {
 	
 	Scheduler scheduler;
+	Config config;
 	
 
 	/**
@@ -28,23 +27,68 @@ public class SchedulerTests {
 	 */
 	@Before
 	public void setUp() throws Exception {
-		Config config = new Config("test.properties");
+		this.config = new Config("test.properties");
 		this.scheduler = new Scheduler(new Logger(config), config);
 	}
 
 	/**
-	 * Testing the scheduler returning a set of destinations that need to be reached given Input 
-	 * object parameters
-	 * 
-	 * Path is an A shape with commands coming incrementally
+	 * Testing that the scheduler will properly return next floors to visit given a collection of floor requests
 	 */
 	@Test
-	public void testFloorSystemScheduleRequestCanGetPickedUp_AShape_SplitRequests() {
+	public void testGetNextFloorsToVisit() {
+		//Add a floor request
+		LinkedList<ScheduledElevatorRequest> floorSystemRequests = new LinkedList<ScheduledElevatorRequest>();
+		floorSystemRequests.add(new ScheduledElevatorRequest(0, 1, true, 9, 0));
+		
+		new Thread(new FakePacketReceiver("Name", this.config.getInt("elevator.port"))).start();
+		this.scheduler.floorSystemScheduleRequest(floorSystemRequests);
+		
+		
+		LinkedList<ElevatorInfo> allElevatorInfos = new LinkedList<ElevatorInfo>();
+		allElevatorInfos.add(new ElevatorInfo(1, 1, 0, null, null));
+		allElevatorInfos.add(new ElevatorInfo(2, 1, 0, null, null));
+		allElevatorInfos.add(new ElevatorInfo(3, 1, 0, null, null));
+		allElevatorInfos.add(new ElevatorInfo(4, 1, 0, null, null));
+		
+		HashMap<Integer,Integer> nextFloors = this.scheduler.getNextFloorsToVisit(allElevatorInfos);
+		
+		for (Integer i : nextFloors.keySet()) {
+			if (nextFloors.get(i) != -1) {
+				//Test successful, a next floor is available
+				return;
+			}
+		}
+		//Only getting here if unsuccessful
+		Assert.fail();
 		
 	}
 	
-	//TODO Show that delayed threads take time to schedule
-	//TODO Show that requests get discarded when everyone is out of service
-	//TODO Show that floor requests lead to elevators having a next floor to visit
-	//TODO Show that there will be negative floors to visit with no requests and out of service elevators
+	/**
+	 * Fake packet receiver that simulates ElevatorSubsystem_SchedulerPacketReceiver but doesn't involve a real ElevatorSubsystem
+	 * @author Millan Wang
+	 *
+	 */
+	private class FakePacketReceiver extends PacketReceiver{
+
+		protected FakePacketReceiver(String name, int port) {
+			super(name, port);
+		}
+
+		@Override
+		protected DatagramPacket createReplyPacketGivenRequestPacket(DatagramPacket requestPacket) {
+			//Create packet to reply with. Then send
+	        byte[] replyData = "200 OK".getBytes();
+	        DatagramPacket replyPacket = new DatagramPacket(replyData, replyData.length, requestPacket.getAddress(), requestPacket.getPort());
+			return replyPacket;
+		}
+	
+		@Override
+		public void run() {
+			System.out.println("Starting " + name + "...");
+			this.sendReply(createReplyPacketGivenRequestPacket(this.receiveNextPacket()));
+			System.out.println("FakePacketReceiver - DONE");
+
+		}
+		
+	}
 }
